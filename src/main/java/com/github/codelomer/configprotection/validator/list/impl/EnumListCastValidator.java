d1f9c0ef254ecc33912010a19bdebb485c33f280
ilmirJava <amaletdinovilmir10@gmail.com>
2024-05-03 01:35:51 +0500

    continue to optimize

---
+++ src/main/java/com/github/codelomer/configprotection/validator/list/impl/EnumListCastValidator.java
@@ -0,0 +1,42 @@
+package com.github.codelomer.configprotection.validator.list.impl;
+
+import com.github.codelomer.configprotection.model.params.impl.ConfigListParams;
+import com.github.codelomer.configprotection.util.ConfigUtil;
+import com.github.codelomer.configprotection.validator.list.ListCastValidator;
+import com.github.codelomer.configprotection.validator.object.impl.EnumObjectCastValidator;
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnumListCastValidator<V extends Enum<V>> implements ListCastValidator<V,String> {
+
+    private final ConfigListParams<V> listParams;
+    private final ConfigUtil configUtil;
+    private final EnumObjectCastValidator<V> enumCastValidator;
+
+    public EnumListCastValidator(@NonNull ConfigListParams<V> listParams, @NonNull ConfigUtil configUtil, @NonNull EnumObjectCastValidator<V> enumCastValidator){
+
+        this.listParams = listParams;
+        this.configUtil = configUtil;
+        this.enumCastValidator = enumCastValidator;
+    }
+    @Override
+    public List<String> getPrimitiveList() {
+        return listParams.getSection().getStringList(listParams.getPath());
+    }
+
+    @Override
+    public List<V> validateList(@NonNull List<String> primitiveList) {
+        List<V> enumList = new ArrayList<>();
+        primitiveList.forEach(name ->{
+            V value = enumCastValidator.cast(name);
+            if(value == null){
+                configUtil.logIllegalArgumentErrorAndReturn(listParams,configUtil.getFullPath(listParams.getSection(),listParams.getPath()));
+                return;
+            }
+            enumList.add(value);
+        });
+        return enumList;
+    }
+}
